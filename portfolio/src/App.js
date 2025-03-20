08bbcd3476e3ca763a0fc588be254b76ac5c5e85
evkrain <123837014+evkrain@users.noreply.github.com>
2025-03-19 21:52:29 -0500

    setup

---
+++ portfolio/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import './components/TypingHeader/TypingHeader.js'
+
+function App() {
+  return (
+    <div className="App">
+      <TypingHeader></TypingHeader>
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio/src/components/TypingHeader/TypingHeader.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+import './TypingHeader.css';
+
+const TypingHeader = () => (
+  <div className="TypingHeader">
+    TypingHeader Component
+  </div>
+);
+
+// TypingHeader.propTypes = {};
+
+TypingHeader.defaultProps = {};
+
+export default TypingHeader;
